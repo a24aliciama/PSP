1c5a1d8d607285b7c4e8509cdf3ad1533db61227
aliciaMA <a24aliciaMA@iessanclemente.net>
2024-09-24 12:36:24 +0200

    runtime

---
+++ Ejercicios/RunTimeExec/src/main/java/org/example/NotepadPlusPlusFile.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.io.IOException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class NotepadPlusPlusFile {
+    public static void main(String[] args) throws IOException {
+
+        Runtime runtime = Runtime.getRuntime();
+        String[] command = {"C:/Program Files/Notepad++/notepad++.exe","./src/main/resources/info.txt"};
+
+        //Process process = runtime.exec(command);  //non fai falta el Process.
+        runtime.exec(command);
+
+        /**
+         * se abre un proceso donde command1 en el padre y command2 se habre dentro del padre
+         */
+
+
+
+
+    }
+}
---
+++ Ejercicios/RunTimeExec/src/main/java/org/example/NotepadPlusplus.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.io.IOException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class NotepadPlusplus {
+    public static void main(String[] args) throws IOException {
+
+        Runtime runtime = Runtime.getRuntime();
+        String[] command = {"C:/Program Files/Notepad++/notepad++.exe"};
+
+        Process process = runtime.exec(command);
+
+
+
+
+    }
+}
