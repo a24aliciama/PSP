90e7d30a851dcbb7615a05b9df19a8e17b7d7f88
dam2 - Martínez Ansede <A24AliciaMA@sanclemente.local>
2025-02-17 12:20:51 +0100

    ..

--- 3.Ejercicios/PSP_UD1/src/main/java/org/Ejercicios1_0/Ejercicio4.java
+++ 3.Ejercicios/PSP_UD1/src/main/java/org/Ejercicios1_0/Ejercicio4.java
@@ -1,13 +1,15 @@
 package org.Ejercicios1_0;
 
-public class Ejercicio4 {
-    /**
-     * DU1 - Review Exercise 4 - Java command line arguments - Stpes
+/**
+ * DU1 - Review Exercise 4 - Java command line arguments - Stpes
 
-     * Create another version of the previous Java program in the same project.
-     * Read input data as command line arguments instead of interactive user input.
-     * Add the new program to the Git repository.
-     * Execute the program from the command line.
-     * Publish the project in GitHub.
-     */
+ * Create another version of the previous Java program in the same project.
+ * Read input data as command line arguments instead of interactive user input.
+ * Add the new program to the Git repository.
+ * Execute the program from the command line.
+ * Publish the project in GitHub.
+ */
+
+public class Ejercicio4 {
+
 }
